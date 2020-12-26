6f18eafed89dd106320c776f39dc35676e87e294
patterncard <patcad@o2.pl>
2020-12-26 10:24:33 +0100

    selecting by id and class

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-    console.log('We are ready!');
-    console.log(document);
-    console.log($(document));
-    $('body').text('Hello world!');
-    $('body').html('<strong>Hello world!</strong>');
+    $('#fire').addClass('highlight');
+    $('#container >>> .non-solid').addClass('highlight');
 });
