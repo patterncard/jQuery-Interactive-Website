f1796464e63de0da5bdfff603cedfc5ca6f0bf00
patterncard <patcad@o2.pl>
2020-12-29 14:29:33 +0100

    change event

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
-    $('.box').on('click', '.box-button', function(){
-        $(this).parent().toggleClass('highlight');
-    });
+    $('#select-menu').on('change', function(){
+        let name = $('#select-menu option:selected').text();
+        let distance = $('#select-menu option:selected').val();
+        let price = $('#select-menu option:selected').data('price');
 
+        if(distance){
+            $('#feedback-message').text('You are signing up for a ' + name + ', which costs ' + price + ', to a distance of ' + distance + ' km');
+        } else {
+            $('#feedback-message').empty();
+        }
+    });
 });
