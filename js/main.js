927dc7f45e8ea5384f808d30b8b8a8c260cec6e3
patterncard <patcad@o2.pl>
2020-12-30 13:02:58 +0100

    preventDefault

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
        let name = $(this).val();
        $('#feedback-message').text('Pleased to meet you ' + name);
     });
+
+    $('a').on('click', function(event){
+        event.preventDefault();
+        $('#feedback-message').text('That\'s fine!');
+    });
 });
