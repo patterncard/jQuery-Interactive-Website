174a50d64167cd449822e0f6aa3402949101cc24
patterncard <patcad@o2.pl>
2020-12-30 13:18:08 +0100

    setting up store items

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
-    $('#input-name').on('keyup', function(){
-       let name = $(this).val();
-       $('#feedback-message').text('Pleased to meet you ' + name);
-    });
-
-    $('a').on('click', function(event){
-        event.preventDefault();
-        $('#feedback-message').text('That\'s fine!');
-    });
+    
 });
