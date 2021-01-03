538a1765db1c9e1617cf726ee6c59df86771e4dc
patterncard <patcad@o2.pl>
2021-01-03 13:41:38 +0100

    creating elements dynamically

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-    
+    $('#button-create-item').on('click', function(){
+        let name = $('#input-create-item').val();
+        $('#input-create-item').val('');
+
+        let html = '';
+        html += '<div class="item">';
+        html += '<div class="name">' + name + '</div>';
+        html += '<img src="assets/pexels-luis-quintero-3689545.jpg" alt="image">';
+        html += '<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus fugiat est, tenetur quidem facere cupiditate beatae ipsum maxime quos accusantium. Atque nobis iusto similique quia magni reiciendis nulla accusantium vero!</div>';
+        html += '<div class="price">499</div>';
+        html += '<button class="item-add">Add to cart</button>';
+        html += '<button class="item-remove">Remove</button>';
+        html += '<br>';
+        html += '<a href="#">More info</a>';
+        html += '<div class="more-info">Lorem ipsum dolor sit</div>';
+        html += '</div>';
+
+        $('#container').prepend(html);
+    });
 });
