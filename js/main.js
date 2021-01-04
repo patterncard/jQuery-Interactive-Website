a749a9a845fb678374227bfdba9a11bc50ac1f60
patterncard <patcad@o2.pl>
2021-01-04 18:26:36 +0100

    removing item, toggle, fade&slide

--- js/main.js
+++ js/main.js
@@ -12,10 +12,24 @@ $(document).ready(function(){
         html += '<button class="item-add">Add to cart</button>';
         html += '<button class="item-remove">Remove</button>';
         html += '<br>';
-        html += '<a href="#">More info</a>';
+        html += '<a class="more-info-link" href="#">More info</a>';
         html += '<div class="more-info">Lorem ipsum dolor sit</div>';
         html += '</div>';
 
         $('#container').prepend(html);
+    });
+
+    $('#container').on('click', '.more-info-link', function(event){
+        event.preventDefault();
+
+        // $(this).parent().find('.more-info').show();
+        // $(this).parent().find('.more-info').toggle();
+        // $(this).parent().find('.more-info').fadeToggle('fast');
+        $(this).parent().find('.more-info').slideToggle('fast');
+
+    });
+
+    $('#container').on('click', '.item-remove', function(){
+        $(this).parent().remove();
     });
 });
