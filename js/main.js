f5c082939267f61a69d2abcf7dfbe9da1174444f
patterncard <patcad@o2.pl>
2021-01-13 12:27:32 +0100

    loading json

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
-$(document).ready(function(){
-    $('#button-create-item').on('click', function(){
-        let name = $('#input-create-item').val();
-        $('#input-create-item').val('');
-
-        let html = '';
+function addItem(name, description, price, moreInfo) {
+    let html = '';
         html += '<div class="item">';
         html += '<div class="name">' + name + '</div>';
         html += '<img src="assets/pexels-luis-quintero-3689545.jpg" alt="image">';
-        html += '<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus fugiat est, tenetur quidem facere cupiditate beatae ipsum maxime quos accusantium. Atque nobis iusto similique quia magni reiciendis nulla accusantium vero!</div>';
-        html += '<div class="price">499</div>';
+        html += '<div class="description">' + description + '</div>';
+        html += '<div class="price">' + price + '</div>';
         html += '<button class="item-add">Add to cart</button>';
         html += '<button class="item-remove">Remove</button>';
         html += '<br>';
-        html += '<a class="more-info-link" href="#">More info</a>';
+        html += '<a class="more-info-link" href="#">' + moreInfo + '</a>';
         html += '<div class="more-info">Lorem ipsum dolor sit</div>';
         html += '</div>';
 
         $('#container').prepend(html);
-    });
+}
 
+$(document).ready(function(){
     $('#container').on('click', '.more-info-link', function(event){
         event.preventDefault();
 
@@ -39,7 +36,17 @@ $(document).ready(function(){
         $(this).parent().remove();
     });
 
-    $.ajax('data/item.json', function(respone){
-        console.log(respone);
-    })
+    $.ajax('data/item.json')
+        .done(function(response){
+            let items = response.items;
+            items.forEach(function(item) {
+                addItem(item.name, item.description, item.price, item.moreInfo);
+            });
+        })
+        .fail(function(request, errorType, errorMessage){
+            console.log(errorMessage);
+        })
+        .always(function(){
+
+        })
 });
